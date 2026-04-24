b18bd994ffa07a6ae65002d26accfd3dae47158e
Benjamin Chausse <benjamin@chausse.xyz>
2026-04-23 22:49:27 -0400

    why must windows use dumbass backslashes???

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,6 +1,7 @@
 --- Most of this setup is to minimize internet bandwith installing
 --- dependencies despite the sandboxed environment. Makefile provides
 --- data paths from the host to grab and load into the sandbox from there.
+local joinpath = vim.fs.joinpath
 local host_nvim_dir = vim.env.HOST_XDG_DATA_HOME
 local deps = {
 	plugins = {
@@ -13,26 +14,26 @@ local deps = {
 	},
 	sources = {
 		-- Preferred: vim.pack directory the host machine
-		{ path = host_nvim_dir .. "/nvim/site/pack/core/opt/", offline = true },
+		{ path = joinpath(host_nvim_dir, "nvim", "site", "pack", "core", "opt"), offline = true },
 		-- Where the CI pipeline clones the dependencies
-		{ path = vim.uv.cwd() .. "/../",                       offline = true },
+		{ path = joinpath(vim.uv.cwd(), ".."),                                   offline = true },
 		-- Last resort: online
-		{ path = "https://github.com/",                        offline = false },
+		{ path = "https://github.com",                                           offline = false },
 	},
 }
 
-	for name, spec in pairs(deps.plugins) do
-		local installed = false
-		for _, src in ipairs(deps.sources) do
-			if installed then break end
-			local full
+for name, spec in pairs(deps.plugins) do
+	local installed = false
+	for _, src in ipairs(deps.sources) do
+		if installed then break end
+		local full
 		if src.offline then
-			full = src.path .. name
+			full = joinpath(src.path, name)
 			if vim.fn.isdirectory(full) ~= 1 then
 				goto continue
 			end
 		else
-			full = src.path .. spec.user .. "/" .. name
+			full = table.concat({ src.path, spec.user, name }, "/")
 			vim.notify('Fetching from online: ' .. full, vim.log.levels.WARN)
 		end
 		local success, err = pcall(function()
@@ -45,7 +46,7 @@ local deps = {
 		else
 			vim.print('Failed to install ' .. name .. ' from ' .. full .. ': ' .. (err or 'unknown error'))
 		end
-			::continue::
+		::continue::
 	end
 end
 
